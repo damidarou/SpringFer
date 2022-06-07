33cc5eda37bc93134760cd9a3a2b3852a628a699
Fernando <fertrillop@gmail.com>
2022-06-07 16:45:24 +0200

    Revert "Test-CasoUso-Proyecto Relacionados y sin repeticiones chungas y arreglamos todo"
    This reverts commit cd3a8e8737cf3b17c6d11d9a8b08a9bf83aae15c.

--- pruebaSpring/src/main/java/com/nigmacode/apirest/controller/PerfilRestController.java
+++ pruebaSpring/src/main/java/com/nigmacode/apirest/controller/PerfilRestController.java
@@ -1,10 +1,12 @@
 package com.nigmacode.apirest.controller;
 
 import com.nigmacode.apirest.entity.Perfil;
-import com.nigmacode.apirest.entity.User;
 import com.nigmacode.apirest.service.PerfilService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,13 +18,7 @@ public class PerfilRestController {
     private PerfilService perfilservice;
     @GetMapping("/perfil")
     public List<Perfil> FindAllPerfil(){
-        List<Perfil>toret=perfilservice.findAll();
-        for (Perfil perfil:toret) {
-            for (User usuario:perfil.getUsuarios()) {
-                usuario.setProyectos(null);
-            }
-        }
-        return toret;
+        return perfilservice.findAll();
     }
     @GetMapping("/perfil/{id}")
     public Optional<Perfil> FindByID(@PathVariable long id){
@@ -32,30 +28,6 @@ public class PerfilRestController {
         }
         return perfil;
     }
-    @PostMapping("/perfil")
-    public Perfil addPerfil(@RequestBody Perfil perfil){
-        perfilservice.save(perfil);
-        perfil.setUsuarios(null);
-        return perfil;
-    }
-    @PutMapping("/perfil")
-    public Perfil updatePerfil(@RequestBody Perfil perfil){
-        perfilservice.save(perfil);
-        perfil.setUsuarios(null);
-        return perfil;
-    }
-    @DeleteMapping("/perfil/{perfilID}")
-    public String borrarPerfil(@PathVariable int perfilID){
-        Optional<Perfil> perfil = perfilservice.findById(Long.valueOf(perfilID));
 
-        if(perfil == null) {
-            throw new RuntimeException("User id not found -"+perfilID);
-        }
-
-        perfilservice.delete(perfilID);
-
-        return "Deleted user id - "+perfilID;
-
-    }
 
 }
